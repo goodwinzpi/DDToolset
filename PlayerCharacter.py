8074690686d50a5ea3f8a2f01ac86672a0878299
goodwinzpi <99686900+goodwinzpi@users.noreply.github.com>
2022-02-15 10:15:45 -0500

    Update PlayerCharacter.py
    Description of features that a player should be able to make use of and initial thoughts on requirements to create them.

--- PlayerCharacter.py
+++ PlayerCharacter.py
@@ -1,1 +1,19 @@
 __Author__ == "goodwinzpi"
+
+"""
+PlayerCharacter should be able to:
+  1. select a race which will contain ability modifiers and racial features (speed, darkvision, etc.)
+  2. select a class (Hit dice, skills, equipment, class features, etc.)
+  3. roll/point buy/arrange ability scores
+  4. select skill proficiencies
+  5. create backstory
+  6. describe appearance
+  7. develop backstory - assisted development via keywords/AI?
+PlayerCharacter will require:
+  1. A database containing the following tables:
+    - Race, Primary Ability, Secondary Ability, Speed, DarkVision, Features,...
+    - Class, Primary Ability, Secondary Ability
+      - Equipment Table
+      - Features Table
+    - Skills, Ability, Description
+"""
